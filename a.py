7fa620249a8d386844977f1081eb0dc9463db76a
Xiaobin Jiang <401236678@qq.com>
2017-12-29 10:33:44 +0800

    词云

--- a.py
+++ a.py
@@ -1,14 +1,51 @@
-a = {}
-b = "aaa"
-if b in a.keys():
-    print("in")
-else:
-    print("not in")
-a[b] = 0
-if b in a.keys():
-    print("in")
-else:
-    print("not in")
+from os import path
+from scipy.misc import imread
+import matplotlib.pyplot as plt
 
-x = "a"
-print(type(x))
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+# 获取当前文件路径
+# __file__ 为当前文件, 在ide中运行此行会报错,可改为
+# d = path.dirname('.')
+d = path.dirname(__file__)
+
+# 读取文本 alice.txt 在包文件的example目录下
+#内容为
+"""
+Project Gutenberg's Alice's Adventures in Wonderland, by Lewis Carroll
+
+This eBook is for the use of anyone anywhere at no cost and with
+almost no restrictions whatsoever.  You may copy it, give it away or
+re-use it under the terms of the Project Gutenberg License included
+with this eBook or online at www.gutenberg.org
+"""
+# text = open(path.join(d, 'alice.txt')).read()
+# print(text)
+# read the mask / color image
+# taken from http://jirkavinse.deviantart.com/art/quot-Real-Life-quot-Alice-282261010
+# 设置背景图片
+alice_coloring = imread(path.join(d, "clip_header.png"))
+
+wc = WordCloud(background_color="white", #背景颜色max_words=2000,# 词云显示的最大词数
+mask=alice_coloring,#设置背景图片
+stopwords=STOPWORDS.add("said"),
+max_font_size=40, #字体最大值
+random_state=42)
+# 生成词云, 可以用generate输入全部文本(中文不好分词),也可以我们计算好词频后使用generate_from_frequencies函数
+txt_freq = {"aaaaa": 1.2,"fvrerfer":6}
+wc.generate_from_frequencies(txt_freq)
+# wc.generate_from_frequencies(txt_freq)
+# txt_freq例子为[('词a', 100),('词b', 90),('词c', 80)]
+# 从背景图片生成颜色值
+image_colors = ImageColorGenerator(alice_coloring)
+
+# 以下代码显示图片
+plt.imshow(wc)
+plt.axis("off")
+# 绘制词云
+# plt.figure()
+# recolor wordcloud and show
+# we could also give color_func=image_colors directly in the constructor
+# plt.imshow(wc.recolor(color_func=image_colors))
+# plt.axis("off")
+plt.show()
